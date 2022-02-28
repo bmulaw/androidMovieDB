d469ef4099f08efd92a257047dd7f408aea9a005
unknown <bmulaw@bu.edu>
2022-02-27 22:34:40 -0500

    modified with new imports

--- app/src/main/java/com/example/movieDB/MovieAdapter.kt
+++ app/src/main/java/com/example/movieDB/MovieAdapter.kt
@@ -1,23 +1,28 @@
 package com.rkpandey.flixster
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.moviedb.R
 
+private const val TAG = "MovieAdapter"
 class MovieAdapter(private val context: Context, private val movies: List<Movie>):
         RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false)
+        Log.i(TAG, "onCreateViewHolder")
+        val view = LayoutInflater.from(context).inflate(R. layout.item_movie, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Log.i(TAG, "onBindViewHolder position $position")
         val movie = movies[position]
         holder.bind(movie)
     }
@@ -32,6 +37,7 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
         fun bind(movie: Movie) {
             movieTitle.text = movie.title
             movieOverview.text = movie.overview
+            Glide.with(context).load(movie.posterImageUrl).into(moviePoster)
         }
     }
 }
